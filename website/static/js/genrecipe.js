f84b78af6f1d9c68ca818e1929f944a32b40d947
Trey Tuscai <ttuscai22@icloud.com>
2025-03-06 17:45:02 -0500

    Updated index.html and added genrecipe.js -- Removed unnecessary files

---
+++ website/static/js/genrecipe.js
@@ -0,0 +1,6 @@
+document.getElementById("recipeForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const ingredients = document.getElementById("ingredients").value;
+
+    
+});
